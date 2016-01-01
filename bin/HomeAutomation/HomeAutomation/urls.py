b5652e10c31057505cdca1277671d94ef38ed160
Breazu Adrian <breazuadrian@gmail.com>
2016-01-01 19:16:35 +0200

    init dashboard app

--- bin/HomeAutomation/HomeAutomation/urls.py
+++ bin/HomeAutomation/HomeAutomation/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^ambient/', include('ambient_data.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- bin/HomeAutomation/ambient_data/views.py
+++ bin/HomeAutomation/ambient_data/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to ambeint_data index page.")
+    return HttpResponse("Welcome to ambient_data index page.")
