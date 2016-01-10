6e699f4daeffd471955652a10c4b92518e9cb795
adrian <breazuadrian@gmail.com>
2016-01-10 15:26:56 +0200

    add morris js chart support

--- bin/HomeAutomation/ambient_data/static/js/morris_detail.js
+++ bin/HomeAutomation/ambient_data/static/js/morris_detail.js
@@ -1,14 +1,11 @@
 var data_array = [];
 for (var i=0; i < jsonDataList.length; i++) {
     var obj = jsonDataList[i];
-    console.log(obj.fields.value);
     if (obj.fields.type == 'temperature')
         data_array.push({date:obj.fields.read_datetime, temperature:obj.fields.value});
     else
         data_array.push({date:obj.fields.read_datetime, humidity:obj.fields.value});
 };
-
-console.log(data_array);
 
 $(Morris.Line({
     element: 'morris-sensor-chart',
