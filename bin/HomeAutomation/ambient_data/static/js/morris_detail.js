a8f0ab851bc415bdf161d5b47a161bf21ca7f16b
adrian <breazuadrian@gmail.com>
2016-02-14 22:24:39 +0200

    show only last 48 hours

--- bin/HomeAutomation/ambient_data/static/js/morris_detail.js
+++ bin/HomeAutomation/ambient_data/static/js/morris_detail.js
@@ -1,13 +1,19 @@
 var temperature_array = [];
 var humidity_array = [];
+var filtered_date = new Date();
+filtered_date = filtered_date.getTime() - 172800000;
+
 /* JSON has data sorted past-> present */
 for (var i=0; i < jsonDataList.length; i++) {
     var obj = jsonDataList[i];
+    var objReadDateTime = new Date(obj.fields.read_datetime);
 
-    if (obj.fields.type == 'temperature')
-        temperature_array.push({date:obj.fields.read_datetime, temperature:obj.fields.value});
-    else
-        humidity_array.push({date:obj.fields.read_datetime, humidity:obj.fields.value});
+    if (objReadDateTime > filtered_date) {
+        if (obj.fields.type == 'temperature')
+            temperature_array.push({date:obj.fields.read_datetime, temperature:obj.fields.value});
+        else
+            humidity_array.push({date:obj.fields.read_datetime, humidity:obj.fields.value});
+    }
 
 };
 
